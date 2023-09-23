2d9251281492b43a727f665e023b1b031b7a30ef
Gabriel Moraes <gcmorais66@gmail.com>
2023-09-23 14:19:26 -0300

    Fix

--- zipzop2.py
+++ zipzop2.py
@@ -22,10 +22,10 @@ def receive_messages(udp_socket, message_ids):
                 message_id = uuid.UUID(message_id.split(" ")[-1])
                 print(f"Confirmação de entrega recebida para a mensagem {message_id}")
         except socket.timeout:
-            print("Tempo limite de recepção. Tentando novamente...")
+            print("")
 
 # Função para enviar mensagens para vários pares com confirmação
-def send_messages(udp_socket_messages, udp_socket_confirmations, peer_addresses, message_ids):
+def send_messages(udp_socket, peer_addresses, message_ids):
     while True:
         message = input("Digite a mensagem a ser enviada (ou 'exit' para sair): ")
         
@@ -39,18 +39,17 @@ def send_messages(udp_socket_messages, udp_socket_confirmations, peer_addresses,
         # Crie uma mensagem formatada com o ID
         message_with_id = f"Message {message_id}: {message}"
         
-        # Enviar a mensagem para todos os pares usando o socket de mensagens
+        # Enviar a mensagem para todos os pares
         for peer_addr in peer_addresses:
-            print("AQUI FOI", message_with_id)
-            udp_socket_messages.sendto(message_with_id.encode('utf-8'), peer_addr)
+            udp_socket.sendto(message_with_id.encode('utf-8'), peer_addr)
             
         # Inicialize um contador para as confirmações
         confirmations = 0
         
-        # Aguarde as confirmações de entrega usando o socket de confirmações
+        # Aguarde as confirmações de entrega
         while confirmations < len(peer_addresses):
             try:
-                data, addr = udp_socket_confirmations.recvfrom(1024)
+                data, addr = udp_socket.recvfrom(1024)
                 confirmation_message = data.decode('utf-8')
                 if confirmation_message.startswith("Confirmation"):
                     confirmation_id = uuid.UUID(confirmation_message.split(" ")[-1])
@@ -63,19 +62,13 @@ def send_messages(udp_socket_messages, udp_socket_confirmations, peer_addresses,
         print("Mensagem entregue com sucesso para todos os pares.")
 
 def main():
-    # Crie dois sockets, um para mensagens e outro para confirmações
-    udp_socket_messages = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket_confirmations = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    udp_socket_messages.settimeout(2)  # Define um timeout de 2 segundos
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.settimeout(2)  # Define um timeout de 2 segundos
 
     my_ip = input("Digite seu endereço IP: ")
-    my_port_messages = int(input("Digite sua porta para mensagens: "))
-    my_port_confirmations = 3333
+    my_port = int(input("Digite sua porta: "))
 
-    # Faça o bind dos sockets nas portas correspondentes
-    udp_socket_messages.bind((my_ip, my_port_messages))
-    udp_socket_confirmations.bind((my_ip, my_port_confirmations))
+    udp_socket.bind((my_ip, my_port))
 
     num_peers = int(input("Quantos pares você deseja adicionar? "))
     peer_addresses = []
@@ -88,11 +81,13 @@ def main():
 
     message_ids = set()
 
-    receive_thread = threading.Thread(target=receive_messages, args=(udp_socket_messages, message_ids))
+    # Crie uma thread para receber mensagens
+    receive_thread = threading.Thread(target=receive_messages, args=(udp_socket, message_ids))
     receive_thread.daemon = True
     receive_thread.start()
 
-    send_thread = threading.Thread(target=send_messages, args=(udp_socket_messages, udp_socket_confirmations, peer_addresses, message_ids))
+    # Crie uma thread para enviar mensagens
+    send_thread = threading.Thread(target=send_messages, args=(udp_socket, peer_addresses, message_ids))
     send_thread.daemon = True
     send_thread.start()
 
@@ -103,9 +98,8 @@ def main():
         if user_input.lower() == 'exit':
             break
 
-    # Feche os sockets ao sair
-    udp_socket_messages.close()
-    udp_socket_confirmations.close()
+    # Feche o socket ao sair
+    udp_socket.close()
 
 if __name__ == "__main__":
     main()
