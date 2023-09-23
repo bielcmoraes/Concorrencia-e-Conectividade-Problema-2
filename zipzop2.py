766fee7f84e1d1788652da4e802a373b06a797e6
Gabriel Moraes <gcmorais66@gmail.com>
2023-09-23 16:08:19 -0300

    Feat: stores all messages in the list

--- zipzop2.py
+++ zipzop2.py
@@ -7,8 +7,8 @@ import platform
 # Cria um Lock
 bloqued = threading.Lock()
 
-# Lista global para armazenar mensagens recebidas
-received_messages = []
+# Lista global para armazenar todas as mensagens
+all_messages = []
 
 # Função para receber mensagens
 def receive_messages(udp_socket, message_ids):
@@ -26,9 +26,8 @@ def receive_messages(udp_socket, message_ids):
                         # Enviar confirmação de entrega da mensagem com o mesmo ID
                         confirmation_message = f"Confirmation {message_id} : Message delivered"
                         udp_socket.sendto(confirmation_message.encode('utf-8'), addr)
-                        #print(f"Mensagem de {addr[0]}:{addr[1]}: {text}")
-                        # Adicione a mensagem à lista global
-                        received_messages.append(f"Mensagem de {addr[0]}:{addr[1]}: {text}")
+                        # Armazenar mensagem na lista global
+                        all_messages.append((addr, text, "Received"))  # Adiciona a etiqueta "Received"
                     except ValueError:
                         print(f"Erro ao analisar o ID da mensagem: {message_id_str}")
             elif message.startswith("Confirmation"):
@@ -64,6 +63,8 @@ def send_messages(udp_socket, peer_addresses, message_ids):
         # Enviar a mensagem para todos os pares
         for peer_addr in peer_addresses:
             udp_socket.sendto(message_with_id.encode('utf-8'), peer_addr)
+        # Armazenar mensagem na lista global
+        all_messages.append(("You", message, "Sent"))  # Adiciona a etiqueta "Sent"
         with bloqued:
             print("Mensagem enviada com sucesso para todos os pares.")
 
@@ -72,23 +73,23 @@ def display_output():
     
     while True:
         with bloqued:
-            user_input = input("Escolha uma opção:\n1. Enviar mensagem\n2. Ler mensagens recebidas\n3. Sair\nOpção: ")
+            user_input = input("Escolha uma opção:\n1. Enviar mensagem\n2. Ler mensagens\n3. Sair\nOpção: ")
         if user_input == '1':
             pass  # Continuar para enviar mensagens
         elif user_input == '2':
-            read_received_messages()  # Chamar a função para ler mensagens recebidas
+            read_messages()  # Chamar a função para ler mensagens
         elif user_input == '3':
             break
         else:
             with bloqued:
                 print("Opção inválida. Tente novamente.")
 
-# Função para ler mensagens recebidas
-def read_received_messages():
+# Função para ler todas as mensagens
+def read_messages():
     with bloqued:
-        print("\nMensagens recebidas:")
-        for idx, message in enumerate(received_messages, start=1):
-            print(f"{idx}. {message}")
+        print("\nTodas as mensagens:")
+        for idx, (sender, text, label) in enumerate(all_messages, start=1):
+            print(f"{idx}. {label} - De {sender[0]}:{sender[1]}: {text}")
         print()
 
 # Função principal
