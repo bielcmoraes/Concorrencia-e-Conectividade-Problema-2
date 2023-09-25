917df8dacf21a1723079d284a1191f16dff83eac
Gabriel Moraes <gcmorais66@gmail.com>
2023-09-25 09:31:07 -0300

    Feat: counts_votes and request_ordered_list

--- zipzop2.py
+++ zipzop2.py
@@ -3,6 +3,7 @@ import threading
 import uuid
 import os
 import platform
+from collections import Counter
 
 # Cria um Lock
 bloqued = threading.Lock()
@@ -10,6 +11,16 @@ bloqued = threading.Lock()
 # Lista global para armazenar todas as mensagens
 all_messages = []
 all_messages_sorted = []
+
+confirmation_messages = {}
+
+# Função para contar o votos em uma lista e retornar o vencedor
+def counts_votes(votes):
+    vote_count = Counter(votes) 
+    max_votes = max(vote_count.values()) 
+    lst = [i for i in vote_count.keys() if vote_count[i] == max_votes]
+    winner = sorted(lst)[0]
+    return winner
 
 # Função para receber mensagens
 def receive_messages(udp_socket, message_ids):
@@ -42,8 +53,23 @@ def receive_messages(udp_socket, message_ids):
                 if len(message_parts) == 4:
                     message_id_str = message_parts[1].strip()
                     position_message = message_parts[3].strip()
+
+                    #Adiciona ao dicionário de mensagens de confirmação
+                    confirmation_messages_exists = confirmation_messages.get(message_id_str)
+
+                    #Salva o posição de cada mensagem na lista de cada par
+                    if confirmation_messages_exists != None:
+                        confirmation_messages[message_id_str].append(position_message)
+                    else:
+                        confirmation_messages[message_id_str] = [position_message]
+                    
+                    print("MENSAGENS DE CONFIRMAÇÃO:", confirmation_messages)
+
                     try:
-                        if position_message == "first": #Verifica se é a primeira mensagem
+                        votes = confirmation_messages.get(message_id_str) #Pega a lista de votos da mensagem a ser processada
+                        voted_position = counts_votes(votes)
+
+                        if voted_position == "first": #Verifica se é a primeira mensagem
                             for message in all_messages: #Verifica se a mensagem foi adicionada na lista de mensagens recebidas
                                 if message[3] == message_id_str:
                                     all_messages_sorted.insert(0, message)
@@ -52,7 +78,7 @@ def receive_messages(udp_socket, message_ids):
                             for message in all_messages:
                                 if message[3] == message_id_str: #Verifica se a mensagem foi adicionada na lista de mensagens recebidas
                                     for message_sorted in all_messages_sorted:
-                                        if message_sorted[3] == position_message: #Encontra a posição de inserção
+                                        if message_sorted[3] == voted_position: #Encontra a posição de inserção
                                             position_isertion_message = all_messages_sorted.index(message_sorted) + 1 #Pega o index da mensagem que eu quero inserir
                                             all_messages_sorted.insert(position_isertion_message, message)
                     except ValueError:
@@ -87,7 +113,35 @@ def send_messages(udp_socket, peer_addresses, message_ids):
         # Armazenar mensagem na lista global
         all_messages.append(("You", message, "Sent", str(message_id)))  # Adiciona a etiqueta "Sent"
 
+# Função para solicitar a lista de mensagens ordenada atual de cada par
+def request_ordered_list(udp_socket, peer_addresses, my_ip):
+    
+    # Gere um novo ID de mensagem
+    message_id = uuid.uuid4()
+    message_with_id = f"UpdateList {message_id} : {my_ip}"
+    
+
+    # Enviar a mensagem para todos os pares
+    for peer_addr in peer_addresses:
+        udp_socket.sendto(message_with_id.encode('utf-8'), peer_addr)
+    
+    udp_socket.settimeout(6) # Defina um timeout de 6s para o socket nessa função
+    
+    updated_lists = []
+
+    while True:
+        try:
+            # Receber a lista de mensagens atualizada de todos os pares e adiciona na lista com todas as respostas
+            data, addr = udp_socket.recvfrom(1024)
+            updated_list = data.decode('utf-8')
+
+            updated_lists.append(updated_list)
         
+        except socket.timeout:
+            break
+    
+    updated_list_winner = counts_votes(updated_lists)
+    return updated_list_winner
 
 # Função para exibir mensagens de saída
 def display_output():
@@ -115,6 +169,9 @@ def read_messages():
 
 # Função principal
 def main():
+
+    global all_messages_sorted
+    
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.settimeout(2)  # Define um timeout de 2 segundos
 
@@ -147,6 +204,9 @@ def main():
 
     receive_thread.start()
     output_thread.start()
+
+    # Solicite as listas ordenadas e atualizadas dos pares conectados
+    all_messages_sorted = request_ordered_list()
 
     # Inicie a thread de envio de mensagens na thread principal
     send_messages(udp_socket, peer_addresses, message_ids)
